5c375212d2a9c102737496868b7c5506302e9234
XIX <xiuming.x@outlook.com>
2020-04-02 12:10:00 -0700

    one App

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-class App extends Component {
-  render () {
-    return <div className='App'>something;</div>;
-  }
-}
+const App = () => {
+  return <div className='App'>something;</div>;
+};
 
-export default connect(function mapStateToProps (state){}, function mapDispatchToProps (dispatch){})(App);
+const mapStateToProps = state => {
+  return {};
+};
+const mapDispatchToProps = dispatch => {
+  //   return {};
+};
+// export default connect(function mapStateToProps (state){}, function mapDispatchToProps (dispatch){})(App);
 
-/* <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header> */
+export default connect(mapStateToProps, mapDispatchToProps)(App);
