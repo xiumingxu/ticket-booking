dd4d8ac4fd49dbc81c95b5a15d72437e68960f99
XIX <xiuming.x@outlook.com>
2020-04-02 11:06:44 -0700

    class App need to add component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
-import store from './store';
-
-import 'normalize.css/normalize.css';
-
-import * as serviceWorker from '../serviceWorker';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,5 +1,17 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-class App extends Component {}
+function App (){
+  return <div className='App'>something;</div>;
+}
 
 export default connect(function mapStateToProps (state){}, function mapDispatchToProps (dispatch){})(App);
+
+/* <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header> */
